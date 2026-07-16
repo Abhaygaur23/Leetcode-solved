f2cedf9129851dd1687040f8ddfc6c7d8f37b53b
Abhaygaur23 <168257824+Abhaygaur23@users.noreply.github.com>
2026-07-16 16:19:40 +0530

    Time: 12 ms (50.95%), Space: 35.2 MB (19.65%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& arr, int k) {
+        if(k>arr.size()){
+            return -1;
+        }
+        long long  end = 0;
+        int start = 0;
+        for(long long i = 0; i< arr.size(); i++){
+            end += arr[i];
+            start = max(start, arr[i]);
+        }
+        long long mid, ans;
+        while(start<= end){
+            mid = start + (end - start)/2;
+            int pages= 0, count = 1;
+            for(int i = 0; i< arr.size(); i++){
+                pages+=arr[i];
+                if(pages>mid){
+                    count++;
+                    pages = arr[i];
+                }
+            }
+            if(count<=k){
+                ans = mid;
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
