61dcc570eeea0b6c91976f2b55b9be2be3865b5d
Abhaygaur23 <168257824+Abhaygaur23@users.noreply.github.com>
2026-07-28 19:55:51 +0530

    Time: 0 ms (100%), Space: 25 MB (88.7%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& arr) {
+        // int n = arr.size();
+        // vector<int> visited(n, 0);
+        // vector<int> ans;
+        // for(int i = 0; i<n; i++){
+        //     int x =  arr[i];
+        //     visited[x-1]++;
+        // }
+        // for(int i = 0; i<n; i++){
+        //     if(visited[i]>1){
+        //         ans.push_back(i+1);
+        //     }
+        // }
+        // for(int i = 0; i<n; i++){
+        //     if(visited[i]==0){
+        //         ans.push_back(i+1);
+        //     }
+        // }
+        // return ans;
+        
         int n = arr.size();
-        vector<int> visited(n, 0);
-        vector<int> ans;
+        vector<int> ans(2);
         for(int i = 0; i<n; i++){
-            int x =  arr[i];
-            visited[x-1]++;
+           arr[i] = arr[i]-1;
         }
         for(int i = 0; i<n; i++){
-            if(visited[i]>1){
-                ans.push_back(i+1);
+            int x = arr[i]%n;
+            arr[x] += n;
+        }
+        for(int i = 0; i<n; i++){
+            int occ = arr[i]/n;
+            if(occ==2){
+                ans[0] = i+1;
             }
-        }
-        for(int i = 0; i<n; i++){
-            if(visited[i]==0){
-                ans.push_back(i+1);
+            if(occ==0){
+                ans[1] = i+1;
             }
         }
         return ans;
