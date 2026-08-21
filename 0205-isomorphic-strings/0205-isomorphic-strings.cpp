956890092a97e7f92421f5bbc68b1e3d3909b1f8
Abhaygaur23 <168257824+Abhaygaur23@users.noreply.github.com>
2026-08-21 21:58:14 +0530

    Time: 4 ms (23.28%), Space: 9.4 MB (58.9%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size())
+            return false;
+
+        unordered_map<char, char> mp1;
+        unordered_map<char, char> mp2;
+
+        for (int i = 0; i < s.size(); i++) {
+            char a = s[i];
+            char b = t[i];
+
+            // s -> t mapping already exists
+            if (mp1.count(a) && mp1[a] != b)
+                return false;
+
+          class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size())
+            return false;
+
+        unordered_map<char, char> mp1;
+        unordered_map<char, char> mp2;
+
+        for (int i = 0; i < s.size(); i++) {
+            char a = s[i];
+            char b = t[i];
+
+            // s -> t mapping already exists
+            if (mp1.count(a) && mp1[a] != b)
+                return false;
+
+            // t -> s mapping already exists
+            if (mp2.count(b) && mp2[b] != a)
+                return false;
+
+            mp1[a] = b;
+            mp2[b] = a;
+        }
+
+        return true;
+    }
+};  // t -> s mapping already exists
+            if (mp2.count(b) && mp2[b] != a)
+                return false;
+
+            mp1[a] = b;
+            mp2[b] = a;
+        }
+
+        return true;
+    }
+};
